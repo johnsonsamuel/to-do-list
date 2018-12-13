8d1eebbdd20c22ed62acfcea246869654467340b
ankita1010 <45850634+ankita1010@users.noreply.github.com>
2018-12-13 19:27:05 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,31 +1,31 @@
 //JavaScript
 
 //storing the data
-
-var obj = {
+    var obj = {
     taskListArr : [],
     taskCompletedArr : []
     };
     
-    //user clicked add button
+//user clicked add button
     var enter = document.getElementById("userInput");
     enter.addEventListener("keyup", 
     function(event)
     {
-     event.preventDefault();
-     if(event.keyCode==13)
-     {document.getElementById('addTaskButton').click();
-     }
+        event.preventDefault();
+        if(event.keyCode==13)
+        {
+            document.getElementById('addTaskButton').click();
+        }
     } );
+    
     document.getElementById("addTaskButton").addEventListener('click', function(){
-     var inputValue = document.getElementById("userInput").value;
+    var inputValue = document.getElementById("userInput").value;
     if(inputValue)
     {
-     
      addTaskFunc(inputValue);
      document.getElementById("userInput").value ="";
      obj.taskListArr.push(inputValue);
-    dataStorageUpdt();
+     dataStorageUpdt();
     }
     
     function dataStorageUpdt()
@@ -40,52 +40,49 @@ var obj = {
      var parent = child.parentNode;
      parent.removeChild(child);
      var id= parent.id;
-    var value = child.innerText;
-    if (id == "taskList")
+     var value = child.innerText;
+     if (id == "taskList")
       {
        obj.taskListArr.splice(obj.taskListArr.indexOf(value), 1);
       }
      else 
       {
       obj.taskCompletedArr.splice(obj.taskCompletedArr.indexOf(value), 1);
-      }
-    
+      } 
      dataStorageUpdt();
     }
-    
-    
+      
     //completed button onclick function
     function completeTask()
     {
       var butId=this.id;
-     var child = this.parentNode.parentNode;
-     var parent = child.parentNode;
-     var ID = parent.id;
-     var value = child.innerText;
-     if (ID == "taskList")
+      var child = this.parentNode.parentNode;
+      var parent = child.parentNode;
+      var ID = parent.id;
+      var value = child.innerText;
+      if (ID == "taskList")
       {
        // to move to completed
        obj.taskListArr.splice(obj.taskListArr.indexOf(value), 1);
-     obj.taskCompletedArr.push(value);
-     this.innerHTML="&#10227;";
+       obj.taskCompletedArr.push(value);
+       this.innerHTML="&#10227;";
       }
      else 
       {
        //to be sent to pending again
-      obj.taskCompletedArr.splice(obj.taskCompletedArr.indexOf(value), 1);
-     obj.taskListArr.push(value);
-     this.innerHTML="&#10004";
+       obj.taskCompletedArr.splice(obj.taskCompletedArr.indexOf(value), 1);
+       obj.taskListArr.push(value);
+       this.innerHTML="&#10004";
       }
     
     dataStorageUpdt();
       var target = (ID=='taskList')?document.getElementById('taskListCompleted'):document.getElementById('taskList');
-    
       parent.removeChild(child);
-       target.insertBefore(child, target.childNodes[0]);
+      target.insertBefore(child, target.childNodes[0]);
     }
     
     
-    ////edit button onclick function
+    //edit button onclick function
     function editTask()
     {
     var parent = this.parentNode;
@@ -104,8 +101,7 @@ var obj = {
     
      var buttonsCont = document.createElement('div');
      buttonsCont.classList.add('buttonContClass');
-    
-      
+   
      var completed = document.createElement('button');
      completed.classList.add('completedButton')
      
@@ -116,10 +112,10 @@ var obj = {
      deleteIt.classList.add('deleteButton')
      
       deleteIt.innerHTML="&#10006";
-    completed.innerHTML="&#10004" ;
-     edit.innerHTML="&#9998";
+      completed.innerHTML="&#10004" ;
+      edit.innerHTML="&#9998";
      
-      buttonsCont.appendChild(taskDiv);
+     buttonsCont.appendChild(taskDiv);
      buttonsCont.appendChild(deleteIt);
      buttonsCont.appendChild(completed);
      buttonsCont.appendChild(edit);
@@ -128,6 +124,7 @@ var obj = {
      var list = document.getElementById("taskList");
      list.insertBefore(task,list.childNodes[0]);
      document.getElementById("userInput").focus();
+        
      //eventlisteners for buttons
      deleteIt.addEventListener('click', deleteTask);
      completed.addEventListener('click', completeTask);
